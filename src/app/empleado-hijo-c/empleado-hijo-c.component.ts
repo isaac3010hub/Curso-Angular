b7dc44016fcdabf8b561ba680384519c91e5545c
IsaacCM_3010 <93792773+isaac3010hub@users.noreply.github.com>
2022-11-28 18:45:37 -0500

    31

---
+++ src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrls: ['./empleado-hijo-c.component.css']
+})
+export class EmpleadoHijoCComponent implements OnInit {
+
+  @Input() empleadoDeLista:Empleado;
+
+  @Input() indice:number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  empleados:Empleado[]=[
+
+    new Empleado("Juan","Díaz","Presidente",7500),
+    new Empleado("Ana","martín","Directora",5500),
+    new Empleado("María","Fdez","Jefa sección",3500),
+    new Empleado("Laura","López","Administrativo",2500),
+
+
+  ];
+
+  empleadoAgregado(empleado:Empleado){
+
+    this.empleados.push(empleado);
+
+  }
+
+  arrayCaracteristicas = [''];
+
+  agregarCaracteristica(nuevaCaracteristica: string){
+    this.arrayCaracteristicas.push(nuevaCaracteristica);
+  }
+
+
+
+}
